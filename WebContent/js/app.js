8aad95b59245c0dc21a92ecc60124d1f864f99f2
sujit <sujitbehera27@gmail.com>
2016-09-09 03:18:13 -0400

    ads

--- WebContent/js/app.js
+++ WebContent/js/app.js
@@ -9,13 +9,13 @@ app.controller('customersCtrl', function($scope,$rootScope, $http) {
 
 app.controller('compDetailCtrl', function($scope, $http) {
 	// Page:Registaration:CompanyDetail :: Submit
-	var name = "userId=";
-	var userid = "";
-    var ca = document.cookie.split(';');
+	
 	$scope.compDetSubmit = function(userDetail){
 		
 		console.log("**********Company Detail Controller*************");
-		
+		var name = "userId=";
+		var userid = "";
+	    var ca = document.cookie.split(';');
 		
 	    for(var i = 0; i <ca.length; i++) {
 	        var c = ca[i];
@@ -28,10 +28,11 @@ app.controller('compDetailCtrl', function($scope, $http) {
 	    }
 	    userid=userid.replace(/"/g,"");
 	    console.log("cookie extracted===>"+userid);
-//	    userDetail.userID = userid;
-//	    console.log("===="+userDetail.userID+"--"+userDetail.userName+"--"+userDetail.companyName);
-	    userDetail.formID = 1;
 	    userDetail.userID = userid;
+	    
+
+	    userDetail.formID = 1;
+	    
 		$http.post("http://localhost:8080/PrecipiceIP/rest/reg/user", userDetail)
 		 .success(function(response){
 			 console.log("=======> " + response);
@@ -46,6 +47,21 @@ app.controller('financeDetailCtrl', function($scope, $http) {
 	$scope.financeDetSubmit = function(userDetail){
 		console.log("***********Finance Management Detail Controller************");
 		userDetail.formID = 2;
+		var name = "userId=";
+		var userid = "";
+	    var ca = document.cookie.split(';');
+		
+	    for(var i = 0; i <ca.length; i++) {
+	        var c = ca[i];
+	        while (c.charAt(0)==' ') {
+	            c = c.substring(1);
+	        }
+	        if (c.indexOf(name) == 0) {
+	        	userid=c.substring(name.length,c.length);
+	        }
+	    }
+	    userid=userid.replace(/"/g,"");
+	    console.log("cookie extracted===>"+userid);
 		$http.post("http://localhost:8080/PrecipiceIP/rest/reg/user", userDetail)
 		 .success(function(response){
 			 console.log("=======> " + response);
@@ -58,6 +74,21 @@ app.controller('businessDetailCtrl', function($scope, $http) {
 	// Page:FinanceDetail :: Submit
 	$scope.businessDetSubmit = function(userDetail){
 		userDetail.formID = 3;
+		var name = "userId=";
+		var userid = "";
+	    var ca = document.cookie.split(';');
+		
+	    for(var i = 0; i <ca.length; i++) {
+	        var c = ca[i];
+	        while (c.charAt(0)==' ') {
+	            c = c.substring(1);
+	        }
+	        if (c.indexOf(name) == 0) {
+	        	userid=c.substring(name.length,c.length);
+	        }
+	    }
+	    userid=userid.replace(/"/g,"");
+	    console.log("cookie extracted===>"+userid);
 		console.log("***********Business Detail Controller************");
 		$http.post("http://localhost:8080/PrecipiceIP/rest/reg/user", userDetail)
 		 .success(function(response){
@@ -72,6 +103,21 @@ app.controller('tradeDetailCtrl', function($scope, $http) {
 	$scope.tradeDetSubmit = function(userDetail){
 		console.log("***********Trade Controller************");
 		userDetail.formID = 4;
+		var name = "userId=";
+		var userid = "";
+	    var ca = document.cookie.split(';');
+		
+	    for(var i = 0; i <ca.length; i++) {
+	        var c = ca[i];
+	        while (c.charAt(0)==' ') {
+	            c = c.substring(1);
+	        }
+	        if (c.indexOf(name) == 0) {
+	        	userid=c.substring(name.length,c.length);
+	        }
+	    }
+	    userid=userid.replace(/"/g,"");
+	    console.log("cookie extracted===>"+userid);
 		$http.post("http://localhost:8080/PrecipiceIP/rest/reg/user", userDetail)
 		 .success(function(response){
 			 console.log("=======> " + response);
@@ -86,6 +132,21 @@ app.controller('riskMngDetailCtrl', function($scope, $http) {
 	$scope.riskMngDetSubmit = function(userDetail){
 		console.log("***********Risk Detail Controller************");
 		userDetail.formID = 5;
+		var name = "userId=";
+		var userid = "";
+	    var ca = document.cookie.split(';');
+		
+	    for(var i = 0; i <ca.length; i++) {
+	        var c = ca[i];
+	        while (c.charAt(0)==' ') {
+	            c = c.substring(1);
+	        }
+	        if (c.indexOf(name) == 0) {
+	        	userid=c.substring(name.length,c.length);
+	        }
+	    }
+	    userid=userid.replace(/"/g,"");
+	    console.log("cookie extracted===>"+userid);
 		$http.post("http://localhost:8080/PrecipiceIP/rest/reg/user", userDetail)
 		 .success(function(response){
 			 console.log("=======> " + response);
