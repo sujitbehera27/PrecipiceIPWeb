bf2c2badd5fe789d2ffff68d0f08564a81490aa0
sujit <sujitbehera27@gmail.com>
2016-09-11 16:47:05 -0400

    changed

---
+++ WebContent/js/controllers/login.controller.js
@@ -0,0 +1,65 @@
+
+	//var application = angular.module('loginRegApp', []);
+    //var loginregapp = angular.module('loginRegApp', []);
+	
+	loginregapp.controller('loginController', function($scope,$rootScope, $http, $location){
+		
+		//Hide & Seek : Login & Reg Div
+		$scope.show = 'false';
+		$rootScope.userId='sujit';
+		$scope.showRegDiv = function(){
+			//$scope.show = $scope.show == 'false' ? 'true' : 'false';
+			$scope.show = 'true';
+			$scope.loginRegistartion = "";
+		}
+		$scope.showLoginDiv=function(){
+	        $scope.show='false';
+	        $scope.loginRegistartion = "";
+	    }
+		//Hide & Seek : End
+		
+		$scope.loginSubmit = function(){
+			 $http.post("http://localhost:8080/PrecipiceIP/rest/user/login", $scope.loginRegistartion)
+			 .success(function(response){
+				 console.log("=======> " + response.firstName);
+				 if(response != null){
+						window.location= './views/reg1.html'
+				 } else{
+					alert("Invalid User Name or Password");
+					$scope.loginRegistartion = "";
+				 }
+			 }); 
+		}
+		
+		// Registration Service Call
+		 $scope.registration = function(){
+			$http.post("http://localhost:8080/PrecipiceIP/rest/user/registarion", $scope.loginRegistartion)
+			 .success(function(response){
+				 console.log("=======> " + response);
+				
+				// $.cookie("userId", response);
+				 $rootScope.userId = response; 
+				 
+				 document.cookie='userId='+response;
+				 //alert($.cookie("userId"));
+				 //TODO: Need to change
+				 if(response != null){
+					 window.location= './views/reg1.html';
+						//window.location= './views/reg1.html?userId='+response
+						//$location.path('./views/reg1.html?userId='+response)
+				 } else{
+					 alert("Registration failed. User ID already exist.");
+					 $scope.loginRegistartion = "";
+				 }
+		 }); 
+			 
+		} 
+		// Forgot Password Service Call
+		/* $scope.forgotPW = function(){
+			console.log("^^^^^^^^^^^^^^^^^^^^^^^^^"+ $scope.loginRegistartion.emailID +"\n and password: " + $scope.loginRegistartion.password);
+			$http.post("http://localhost:8080/PrecipiceIP/rest/user/forgotPassword", $scope.loginRegistartion)  //TODO : Set the email ID Only.
+			 .success(function(response){
+				 console.log("=======> " + response);
+			 });
+		} */
+	});
