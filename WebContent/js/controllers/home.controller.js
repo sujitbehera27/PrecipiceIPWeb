d3f99140a0649bd89fa2f77c79df88ee04f79d9f
sujit <sujitbehera27@gmail.com>
2016-09-13 01:43:45 -0400

    added more mappings to the rootscope variable

--- WebContent/js/controllers/home.controller.js
+++ WebContent/js/controllers/home.controller.js
@@ -22,11 +22,11 @@ homeApp.controller('compDetailCtrl', function($scope,$rootScope, $http) {
 	    userDetail.userID = $rootScope.userId;
 	    
 
-	    userDetail.formID = 1;
+	    userDetail.formID = 2;
 //	    $scope.lclUserDetail = userDetail;
 	    //userdetService.set(userDetail);
 	    $rootScope.userDetail = userDetail;
-		$http.post("http://localhost:8080/PrecipiceIP/rest/reg/user", userDetail)
+		$http.post("http://localhost:8080/PrecipiceIP/rest/reg/user", $rootScope.userDetail)
 		 .success(function(response){
 			 console.log("=======> " + response);
 //			 $scope.lclUserDetail = userDetail;
@@ -72,8 +72,16 @@ homeApp.controller('businessDetailCtrl', function($scope,$rootScope, $http) {
 	$scope.businessDetSubmit = function(userDetail){
 		userDetail.formID = 3;
 		userDetail.userID = $rootScope.userId;
+		$rootScope.userDetail.formID = 2;
+		$rootScope.userDetail.compOverall = userDetail.compOverall;
+		$rootScope.userDetail.compByBu = userDetail.compByBu;
+		$rootScope.userDetail.compMarketAct = userDetail.compMarketAct;
+		$rootScope.userDetail.compIpPolicy = userDetail.compIpPolicy;
+		$rootScope.userDetail.compIpStrategy = userDetail.compIpStrategy;
+		$rootScope.userDetail.businesStrategy = userDetail.businesStrategy;
+		
 		console.log("***********Business Detail Controller************");
-		$http.post("http://localhost:8080/PrecipiceIP/rest/reg/user", userDetail)
+		$http.post("http://localhost:8080/PrecipiceIP/rest/reg/user", $rootScope.userDetail)
 		 .success(function(response){
 			 console.log("=======> " + response);
 		//	 $window.location.href= './tradedet.html'
@@ -81,13 +89,25 @@ homeApp.controller('businessDetailCtrl', function($scope,$rootScope, $http) {
 	}
 });
 
-homeApp.controller('tradeDetailCtrl', function($scope, $http) {
+homeApp.controller('tradeDetailCtrl', function($scope,$rootScope, $http) {
 	// Page:TradeDetails :: Submit
 	$scope.tradeDetSubmit = function(userDetail){
 		console.log("***********Trade Controller************");
 		userDetail.formID = 4;
 		userDetail.userID = $rootScope.userId;
-		$http.post("http://localhost:8080/PrecipiceIP/rest/reg/user", userDetail)
+		$rootScope.userDetail.formID = 2;
+		$rootScope.userDetail.preserveCopyRight = userDetail.preserveCopyRight;
+		$rootScope.userDetail.preserveCopyRight=userDetail.preserveCopyRight;
+		$rootScope.userDetail.regTradeMark=userDetail.regTradeMark;
+		$rootScope.userDetail.domainRefBN=userDetail.domainRefBN;
+		$rootScope.userDetail.domainRefPN=userDetail.domainRefPN;
+		$rootScope.userDetail.tradeSec=userDetail.tradeSec;
+		$rootScope.userDetail.categoriTradeSec=userDetail.categoriTradeSec;
+		$rootScope.userDetail.mngCyberSecRisk = userDetail.mngCyberSecRisk
+		$rootScope.userDetail.protectTradeSec = userDetail.protectTradeSec;
+		$rootScope.userDetail.protectConfInfo = userDetail.protectConfInfo;
+		
+		$http.post("http://localhost:8080/PrecipiceIP/rest/reg/user", $rootScope.userDetail)
 		 .success(function(response){
 			 console.log("=======> " + response);
 			// $window.location.href= './riskdet.html'
@@ -96,13 +116,25 @@ homeApp.controller('tradeDetailCtrl', function($scope, $http) {
 });
 
 
-homeApp.controller('riskMngDetailCtrl', function($scope, $http) {
+homeApp.controller('riskMngDetailCtrl', function($scope,$rootScope, $http) {
 	// Page:Risk Management :: Submit
 	$scope.riskMngDetSubmit = function(userDetail){
 		console.log("***********Risk Detail Controller************");
 		userDetail.formID = 5;
 		userDetail.userID = $rootScope.userId;
-		$http.post("http://localhost:8080/PrecipiceIP/rest/reg/user", userDetail)
+		$rootScope.userDetail.formID = 2;
+		$rootScope.userDetail.hasConfInfo = userDetail.hasConfInfo;
+		$rootScope.userDetail.hasIpPolicy = userDetail.hasIpPolicy;
+		$rootScope.userDetail.agreementMngProt = userDetail.agreementMngProt;
+		$rootScope.userDetail.agreementMngProt = userDetail.agreementMngProt;
+		$rootScope.userDetail.hasErm = userDetail.hasErm;
+		$rootScope.userDetail.thirdPartyLia = userDetail.thirdPartyLia;
+		$rootScope.userDetail.selfInsIpLose = userDetail.selfInsIpLose;
+		$rootScope.userDetail.purchedThirdPartyIns = userDetail.purchedThirdPartyIns;
+		$rootScope.userDetail.hasOverIpGov = userDetail.hasOverIpGov;
+		
+		
+		$http.post("http://localhost:8080/PrecipiceIP/rest/reg/user", $rootScope.userDetail)
 		 .success(function(response){
 			 console.log("=======> " + response);
 			// $window.location.href= './financedet.html'
